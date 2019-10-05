fc0e7a82aabfcba260b87337eeca898951d4a33e
Calvin Rose <calsrose@gmail.com>
2019-10-04 22:04:38 -0500

    Add symmetric encryption bindings.
    Libhydrogen exposes a secretbox interface that uses symmetric
    encryption.

--- jhydro.c
+++ jhydro.c
@@ -28,6 +28,8 @@
 /* Utils */
 /*********/
 
+/* Get an optional buffer for keygen functions, and ensure it has capacity for
+ * len bytes */
 static JanetBuffer *util_keygen_prep(int32_t argc, const Janet *argv, int len) {
     janet_arity(argc, 0, 1);
     JanetBuffer *buffer;
@@ -42,6 +44,15 @@ static JanetBuffer *util_keygen_prep(int32_t argc, const Janet *argv, int len) {
         }
     }
     return buffer;
+}
+
+/* Get a byte view with at least nbytes bytes. Otherwise the same janet_getbytes. */
+static JanetByteView util_getnbytes(const Janet *argv, int32_t n, int nbytes) {
+    JanetByteView view = janet_getbytes(argv, n);
+    if (view.len < nbytes) {
+        janet_panicf("slot %d expected at least %d bytes, got %d", n, nbytes, view.len);
+    }
+    return view;
 }
 
 /****************************/
@@ -110,10 +121,7 @@ static Janet cfun_random_buf_deterministic(int32_t argc, Janet *argv) {
     JanetBuffer *buf = janet_getbuffer(argv, 0);
     size_t len = janet_getsize(argv, 1);
     if (len > INT32_MAX) janet_panic("size too large");
-    JanetByteView seed = janet_getbytes(argv, 2);
-    if (seed.len < hydro_random_SEEDBYTES) {
-        janet_panicf("seed must be at least %d bytes", hydro_random_SEEDBYTES);
-    }
+    JanetByteView seed = util_getnbytes(argv, 2, hydro_random_SEEDBYTES);
     janet_buffer_ensure(buf, buf->count + len, 2);
     hydro_random_buf_deterministic(buf->data + buf->count, len, seed.bytes);
     buf->count += len;
@@ -143,14 +151,8 @@ static const JanetAbstractType HashState = {
 
 static Janet cfun_hash_new(int32_t argc, Janet *argv) {
     janet_fixarity(argc, 2);
-    JanetByteView ctx = janet_getbytes(argv, 0);
-    JanetByteView key = janet_getbytes(argv, 1);
-    if (ctx.len < hydro_hash_CONTEXTBYTES) {
-        janet_panicf("ctx too short, expected at least %d bytes", hydro_hash_CONTEXTBYTES);
-    }
-    if (key.len < hydro_hash_KEYBYTES) {
-        janet_panicf("key too short, expected at least %d bytes", hydro_hash_KEYBYTES);
-    }
+    JanetByteView ctx = util_getnbytes(argv, 0, hydro_hash_CONTEXTBYTES);
+    JanetByteView key = util_getnbytes(argv, 1, hydro_hash_KEYBYTES);
     hydro_hash_state *state = janet_abstract(&HashState, sizeof(hydro_hash_state));
     int result = hydro_hash_init(state, ctx.bytes, key.bytes);
     if (result) {
@@ -185,6 +187,82 @@ static Janet cfun_hash_final(int32_t argc, Janet *argv) {
     return janet_wrap_string(janet_string_end(out));
 }
 
+/**************/
+/* Secret Box */
+/**************/
+
+static Janet cfun_secretbox_keygen(int32_t argc, Janet *argv) {
+    JanetBuffer *buffer = util_keygen_prep(argc, argv, hydro_secretbox_KEYBYTES);
+    hydro_secretbox_keygen(buffer->data);
+    return janet_wrap_buffer(buffer);
+}
+
+static Janet cfun_secretbox_encrypt(int32_t argc, Janet *argv) {
+    janet_arity(argc, 4, 5);
+    JanetByteView msg = janet_getbytes(argv, 0);
+    uint64_t msg_id = (uint64_t) janet_getinteger64(argv, 1);
+    JanetByteView ctx = util_getnbytes(argv, 2, hydro_secretbox_CONTEXTBYTES);
+    JanetByteView key = util_getnbytes(argv, 3, hydro_secretbox_KEYBYTES);
+    JanetBuffer *cipher;
+    if (argc == 5) {
+        cipher = janet_getbuffer(argv, 4);
+        janet_buffer_ensure(cipher,
+                cipher->count + msg.len + hydro_secretbox_HEADERBYTES, 2);
+    } else {
+        cipher = janet_buffer(msg.len + hydro_secretbox_HEADERBYTES);
+    }
+    int result = hydro_secretbox_encrypt(cipher->data + cipher->count,
+            msg.bytes, msg.len, msg_id, ctx.bytes, key.bytes);
+    if (result) {
+        janet_panic("encryption failed");
+    }
+    cipher->count += msg.len + hydro_secretbox_HEADERBYTES;
+    return janet_wrap_buffer(cipher);
+}
+
+static Janet cfun_secretbox_decrypt(int32_t argc, Janet *argv) {
+    janet_arity(argc, 4, 5);
+    JanetByteView ciphertext = janet_getbytes(argv, 0);
+    uint64_t msg_id = (uint64_t) janet_getinteger64(argv, 1);
+    JanetByteView ctx = util_getnbytes(argv, 2, hydro_secretbox_CONTEXTBYTES);
+    JanetByteView key = util_getnbytes(argv, 3, hydro_secretbox_KEYBYTES);
+    JanetBuffer *msg;
+    if (argc == 5) {
+        msg = janet_getbuffer(argv, 4);
+        janet_buffer_ensure(msg,
+                msg->count + ciphertext.len - hydro_secretbox_HEADERBYTES, 2);
+    } else {
+        msg = janet_buffer(ciphertext.len - hydro_secretbox_HEADERBYTES);
+    }
+    int result = hydro_secretbox_decrypt(msg->data + msg->count,
+            ciphertext.bytes, ciphertext.len, msg_id, ctx.bytes, key.bytes);
+    if (result) {
+        janet_panic("decryption failed");
+    }
+    msg->count += ciphertext.len - hydro_secretbox_HEADERBYTES;
+    return janet_wrap_buffer(msg);
+}
+
+static Janet cfun_secretbox_probe_create(int32_t argc, Janet *argv) {
+    janet_fixarity(argc, 3);
+    JanetByteView c = janet_getbytes(argv, 0);
+    JanetByteView ctx = util_getnbytes(argv, 1, hydro_secretbox_CONTEXTBYTES);
+    JanetByteView key = util_getnbytes(argv, 2, hydro_secretbox_KEYBYTES);
+    uint8_t *probe = janet_string_begin(hydro_secretbox_PROBEBYTES);
+    hydro_secretbox_probe_create(probe, c.bytes, c.len, ctx.bytes, key.bytes);
+    return janet_wrap_string(janet_string_end(probe));
+}
+
+static Janet cfun_secretbox_probe_verify(int32_t argc, Janet *argv) {
+    janet_fixarity(argc, 4);
+    JanetByteView probe = util_getnbytes(argv, 0, hydro_secretbox_PROBEBYTES);
+    JanetByteView c = janet_getbytes(argv, 1);
+    JanetByteView ctx = util_getnbytes(argv, 2, hydro_secretbox_CONTEXTBYTES);
+    JanetByteView key = util_getnbytes(argv, 2, hydro_secretbox_KEYBYTES);
+    return janet_wrap_boolean(
+            !hydro_secretbox_probe_verify(probe.bytes, c.bytes, c.len, ctx.bytes, key.bytes));
+}
+
 /*******/
 /* KDF */
 /*******/
@@ -205,14 +283,8 @@ static Janet cfun_kdf_derive_from_key(int32_t argc, Janet *argv) {
         janet_panicf("subkey length must be at most %d, got %d",
             hydro_kdf_BYTES_MAX, subkey_len);
     uint64_t subkey_id = (uint64_t) janet_getinteger64(argv, 1);
-    JanetByteView ctx = janet_getbytes(argv, 2);
-    if (ctx.len < hydro_kdf_CONTEXTBYTES)
-        janet_panicf("ctx must be at least %d bytes, got %d bytes",
-                hydro_kdf_CONTEXTBYTES, ctx.len);
-    JanetByteView key = janet_getbytes(argv, 3);
-    if (key.len < hydro_kdf_KEYBYTES)
-        janet_panicf("key must be at least %d bytes, got %d bytes",
-                hydro_kdf_KEYBYTES, key.len);
+    JanetByteView ctx = util_getnbytes(argv, 2, hydro_kdf_CONTEXTBYTES);
+    JanetByteView key = util_getnbytes(argv, 3, hydro_kdf_KEYBYTES);
     uint8_t *subkey = janet_string_begin(subkey_len);
     int result = hydro_kdf_derive_from_key(subkey, subkey_len, subkey_id, ctx.bytes, key.bytes);
     if (result) {
@@ -220,7 +292,6 @@ static Janet cfun_kdf_derive_from_key(int32_t argc, Janet *argv) {
     }
     return janet_wrap_string(janet_string_end(subkey));
 }
-
 /****************/
 /* Module Entry */
 /****************/
@@ -228,44 +299,70 @@ static Janet cfun_kdf_derive_from_key(int32_t argc, Janet *argv) {
 static const JanetReg cfuns[] = {
 
     /* Random */
-    {"random/u32", cfun_random_u32, "(hydro/random/u32)\n\n"
+    {"random/u32", cfun_random_u32, "(jhydro/random/u32)\n\n"
         "Generate a psuedo random 32 bit unsigned integer"},
-    {"random/uniform", cfun_random_uniform, "(hydro/random/uniform top)\n\n"
+    {"random/uniform", cfun_random_uniform, "(jhydro/random/uniform top)\n\n"
         "Generate a random 32 bit unsigned integer less than top."},
-    {"random/buf", cfun_random_buf, "(hydro/random/buf buf &opt size)\n\n"
+    {"random/buf", cfun_random_buf, "(jhydro/random/buf buf &opt size)\n\n"
         "Fill a buffer with random bytes. If size is not provided, will clear "
         "and fill the given buffer. If size is provided, will append size random "
         "bytes to the buffer. Alternatively, you can provide just the size argument, and "
         "a new randomized buffer will be returned."},
-    {"random/ratchet", cfun_random_ratchet, "(hydro/random/ratchet)\n\n"
+    {"random/ratchet", cfun_random_ratchet, "(jhydro/random/ratchet)\n\n"
         "Increment the internal state of the RNG."},
-    {"random/reseed", cfun_random_reseed, "(hydro/random/reseed)\n\n"
+    {"random/reseed", cfun_random_reseed, "(jhydro/random/reseed)\n\n"
         "Provide a new random seed for the internal RNG."},
     {"random/buf-deterministic", cfun_random_buf_deterministic,
-        "(hydro/random/buf-deterministic buf len seed)\n\n"
-            "Generate len randon bytes and push them into a buffer buf. seed "
+        "(jhydro/random/buf-deterministic buf len seed)\n\n"
+            "Generate len random bytes and push them into a buffer buf. seed "
             "is a byte sequence of at least 32 bytes that initializes the state of the RNG. "
             "Returns the modified buffer."},
     /* Hashing */
-    {"hash/keygen", cfun_hash_keygen, "(hydro/hash/keygen &opt buf)\n\n"
+    {"hash/keygen", cfun_hash_keygen, "(jhydro/hash/keygen &opt buf)\n\n"
         "Generate a key suitable for use in hashing. The key is a buffer of at "
         "least 32 bytes. If a buffer buf is provided, the first 32 bytes of buf "
         "will be set to a new random key. Returns a key buffer."},
-    {"hash/new", cfun_hash_new, "(hydro/hash/new ctx key)\n\n"
+    {"hash/new", cfun_hash_new, "(jhydro/hash/new ctx key)\n\n"
         "Create a new hash-state. Takes a context ctx and a key and returns a new abstract type, "
         "jhydro/hash-state. Both ctx and key should be byte sequences, of at least lengths 8 and 32 "
         "respectively. Returns the new state."},
-    {"hash/update", cfun_hash_update, "(hydro/hash/update state bytes)\n\n"
+    {"hash/update", cfun_hash_update, "(jhydro/hash/update state bytes)\n\n"
         "Add more bytes to the hash state. Returns the modified state"},
-    {"hash/final", cfun_hash_final, "(hydro/hash/final state len)\n\n"
+    {"hash/final", cfun_hash_final, "(jhydro/hash/final state len)\n\n"
         "Get the final hash after digesting all of the input as a string. The resulting "
         "hash will be a string of length len."},
+    /* Secret Box - symmetric encryption */
+    {"secretbox/keygen", cfun_secretbox_keygen, "(jyhdro/secretbox/keygen)\n\n"
+        "Generate a key suitable for secretbox. The returned key is a 32 byte buffer."},
+    {"secretbox/encrypt", cfun_secretbox_encrypt,
+        "(jhydro/secretbox/encrypt msg msg-id ctx key &opt buf)\n\n"
+        "Encrypt a message with a secretbox key and return the cipher text in a buffer. "
+        "Also requires a message id, which is an integer, and a ctx, which is a non-secret "
+        "byte-sequence. Lastly, requires a secret symmetric key for encryption. An optional "
+        "buffer will prevent Janet from creating a new buffer, and instead append to and return "
+        "the provided buffer."},
+    {"secretbox/decrypt", cfun_secretbox_decrypt,
+        "(jhydro/secretbox/decrypt cipher-text msg-id ctx key &opt buf)\n\n"
+        "Decrypt a cipher text that was produced with jhydro/secretbox/encrypt. msg-id, "
+        "ctx, and key must be the same as those used to encrypt the message. An optional "
+        "buffer can be used to contain the plain text, otherwise a new buffer is created. "
+        "Returns a buffer containing the plain text."},
+    {"secretbox/probe-create", cfun_secretbox_probe_create,
+        "(jhydro/secretbox/probe-create cipher-text ctx key)\n\n"
+        "Create a probe for some cipher text created by jhydro/secretbox/encrypt. The "
+        "resulting probe is a constant length string that can be used to verify if cipher text "
+        "is valid before decrypting the entire text. Returns a string."},
+    {"secretbox/probe-verify", cfun_secretbox_probe_verify,
+        "(jhydro/secretbox/probe-verify probe cipher-text ctx key)\n\n"
+        "Use a probe produced by jhydro/secretbox/probe-create to check if some cipher text "
+        "is genuine. If the cipher text is not forged or tampered with, returns true, otherwise "
+        "false. Genuine cipher text can then be decrypted. Returns a boolean."},
     /* KDF */
-    {"kdf/keygen", cfun_kdf_keygen, "(hydro/kdf/keygen &opt buf)\n\n"
+    {"kdf/keygen", cfun_kdf_keygen, "(jhydro/kdf/keygen &opt buf)\n\n"
         "Generate a key for use in KDFs. Returns the modified buf if provided, or "
         "a new random buffer."},
     {"kdf/derive-from-key", cfun_kdf_derive_from_key,
-        "(hydro/kdf/derive-from-key sublen subid ctx key)\n\n"
+        "(jhydro/kdf/derive-from-key sublen subid ctx key)\n\n"
         "Generate a subkey from a master key. Takes a subid, which is "
         "a positive integer that represents the key id, and ctx, which is "
         "an 8 byte string that is usually an application constant. Finally, the "
@@ -275,5 +372,5 @@ static const JanetReg cfuns[] = {
 
 JANET_MODULE_ENTRY(JanetTable *env) {
     hydro_init();
-    janet_cfuns(env, "hydro", cfuns);
+    janet_cfuns(env, "jhydro", cfuns);
 }
